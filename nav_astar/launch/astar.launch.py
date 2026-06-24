37de118160ec5e500f002e0cb7dd9c13336023b7
janduigorila <janduigamer13@gmail.com>
2026-06-24 00:43:55 -0300

    alterações para funcionamento do build

--- nav_astar/launch/astar.launch.py
+++ nav_astar/launch/astar.launch.py
@@ -1,4 +1,8 @@
 import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 from ament_index_python.packages import (
     get_package_share_directory
@@ -18,6 +22,8 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     pkg_bringup = get_package_share_directory(
         'nav_bringup'
@@ -62,7 +68,17 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false'
+        ),
+        Node(
+        package='nav_astar',
+        executable='astar_node',
+        parameters=[{'use_sim_time': use_sim_time}]
+        ),
         sim,
         slam,
-        astar
+        astar,
+        
     ])
--- nav_astar/setup.py
+++ nav_astar/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/astar.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +25,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'astar_node = nav_astar.astar_node:main',
         ],
     },
 )
